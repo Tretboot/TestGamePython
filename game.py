854f6610bc0d6248c59a173782e7d059de848e37
Tretboot <37165913+Tretboot@users.noreply.github.com>
2023-04-11 10:53:33 +0200

    Update game.py
    debugged some, to the point that it sort of does something

--- game.py
+++ game.py
@@ -1,18 +1,17 @@
 import random
-
-def main():
-print("Willst du gewinnen?")
-antwort = input("Ja oder Nein? ")
-if antwort == "Ja":
-print("Herzlichen Glückwunsch! Du hast gewonnen!")
-else:
-while True:
 fragen = ["Warum nicht?", "Hast du das Spiel verstanden?", "Wie geht es dir heute?"]
-print(random.choice(fragen))
+def main():
+    print("Willst du gewinnen?")
 antwort = input("Ja oder Nein? ")
 if antwort == "Ja":
-print("Herzlichen Glückwunsch! Du hast gewonnen!")
+    print("Herzlichen Glückwunsch! Du hast gewonnen!")
+else:
+    while True:
+            print(random.choice(fragen))
+antwort = input("Ja oder Nein? ")
+if antwort == "Ja":
+    print("Herzlichen Glückwunsch! Du hast gewonnen!")
 break
 
 if __name__ == "__main__":
-main()
+    main()
