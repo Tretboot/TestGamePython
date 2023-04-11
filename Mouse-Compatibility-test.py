dc72a87e644f4d5ecb5aad7db2ee99022d909ef5
Tretboot <37165913+Tretboot@users.noreply.github.com>
2023-04-11 10:44:08 +0200

    Update Mouse-Compatibility-test.py
    cleaned up code a bit

--- Mouse-Compatibility-test.py
+++ Mouse-Compatibility-test.py
@@ -1,16 +1,16 @@
 import mouse
 
 # Überprüfen Sie, ob die Maus angeschlossen ist
-if mouse.is_pressed(button=';left';):
-print(';Maus ist angeschlossen';)
+if mouse.is_pressed(button='left'):
+  print('Maus ist angeschlossen')
 else:
-print(';Maus ist nicht angeschlossen';)
+  print('Maus ist nicht angeschlossen')
 
 # Bewegen Sie die Maus um 100 Pixel nach rechts und unten mit einer Dauer von 0,5 Sekunden
 mouse.move(100, 100, absolute=False, duration=0.5)
 
 # Klicken Sie mit der linken Maustaste
-mouse.click(';left';)
+mouse.click('left')
 
 # Klicken Sie mit der rechten Maustaste
-mouse.click(';right';)
+mouse.click('right')
